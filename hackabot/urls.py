0b15c9bd255bd8b865a0377607929375ab5c1fbe
James Potter <james.potter@gmail.com>
2026-01-05 12:47:15 +0100

    Add root URL redirect to hacka.network

--- hackabot/urls.py
+++ hackabot/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 from hackabot.apps.bot.views import api_nodes, telegram_webhook
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="https://hacka.network")),
     path("admin/", admin.site.urls),
     path("webhook/telegram/", telegram_webhook, name="telegram_webhook"),
     path("api/nodes/", api_nodes, name="api_nodes"),
